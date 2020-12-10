5abd05eafacfac449fee9d83018de37f787d93e5
Tinnys <a.tthazikry097@gmail.com>
2020-12-10 08:59:28 +0700

    Memperbaiki math.js

--- commands/utility/math.js
+++ commands/utility/math.js
@@ -8,8 +8,22 @@ module.exports = {
   args: true,
   usage: '<expression> e.g., 2+2*(9/sqrt(9))',
   execute (message, args) {
-    let expression
+    /*
+     * Issue (1): Memory Leak when use ":" operator.
+     * Solution: limit the number until 99.
+     */
+    let item = args[0]
+    const condition = item.indexOf(':') === 1;
+    if (condition) {
+      if (!(item.startsWith(1))) return
+      const amount = item.split(':').slice(1)
+      if (amount > 100) {
+        message.channel.send('Sorry, only 99')
+        return 
+      }
+    }
 
+    let expression
     try {
       expression = math.evaluate(args.join(' '))
     } catch (e) {
@@ -19,7 +33,6 @@ module.exports = {
     const embed = new MessageEmbed()
       .setColor(0xffffff)
       .addField('Output', `\`\`\`\n${expression}\`\`\``)
-
     message.channel.send(embed)
   }
 }
